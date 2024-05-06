4bb255b5a0bfa6727627ba12f2abc3f6451f3703
antonbil23 <anbilonog@gmail.com>
2024-05-05 20:31:42 -0400

    yeahhhhh

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logo from './nba-logo.png';
+import ToggleMenu from './toggle.js';
 
 function App() {
   return (
     <div className="App">
+      <ToggleMenu />
+      <div className="Home-header">
+        <div>
+        <img src={logo} className="NBA-logo" alt="Nba Logostyle" />
+        </div>
+        <div className="title">
+        NBA Sports Betting Prediction Model
+        </div>
+      </div>
+      
+      <div className="authors">
+                By: 
+                <a className="App-link" href="https://www.linkedin.com/in/rahul-aneja-746ba5207/"> Rahul Aneja</a>, 
+                <a className="App-link" href="https://www.linkedin.com/in/gioromruiz/"> Gio Romero-Ruiz</a>, 
+                <a className="App-link" href="https://www.linkedin.com/in/antonbilonog/"> Anton Bilonog</a>
+            </div>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to relooooood.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -18,6 +36,8 @@ function App() {
           Learn React
         </a>
       </header>
+
+
     </div>
   );
 }
